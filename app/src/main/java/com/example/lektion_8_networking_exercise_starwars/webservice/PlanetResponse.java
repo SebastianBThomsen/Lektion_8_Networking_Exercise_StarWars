aaef6c37d7cd7c809df4893ff0dfa3b19ac3da52
Sebastian <280301@via.dk>
2022-05-04 00:58:12 +0200

    Spørgsmål til Kasper udvidet

--- app/src/main/java/com/example/lektion_8_networking_exercise_starwars/webservice/PlanetResponse.java
+++ app/src/main/java/com/example/lektion_8_networking_exercise_starwars/webservice/PlanetResponse.java
@@ -4,6 +4,7 @@ public class PlanetResponse {
     private String name, climate, gravity, terrain;
     private int rotation_period, orbital_period, diameter;
     //private int surface_Water //FIXME: Spørg kasper hvorfor dette ikke virker på person 33? (Linke 70-71 StarWarsRepository)
+    // - Kan man gøre andet end at lave det hele til strings?
     private double population;
 
 
